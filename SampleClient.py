2ab83284954e0e0b853a627206bc8de063ad2a1c
mclarkk <meghan.leah.clark@gmail.com>
2014-11-11 23:26:17 -0500

    some cleanup and futzing with relative imports

--- SampleClient.py
+++ SampleClient.py
@@ -1,4 +1,4 @@
-from S2SimSession import S2SimSession
+from .lib import S2SimSession #import S2SimSession
 from time import sleep
 import sys
 
@@ -9,9 +9,7 @@ BUFFER_SIZE = 1024
 # Upon first connection, sender id = 0xFFFF. The sender_id to use for subsequent msgs will
 # be scraped from the sender_id field of a successful connection response from the server
 new_sender_id = "FFFF"
-sender_id = None
-
-threshold_price = 300 
+sender_id = None 
 
 s2sim_debug = False 
 
@@ -31,14 +29,12 @@ def main():
 		while(True):
 
 			# Wait for price signal
-			price_set = False
 			interval_price = None
-			while(not price_set):
+			while(interval_price == None):
 				msg = session.wait_for_server()
 				if msg != None:
 					if msg.__class__.__name__ == "SetCurrentPriceMsg":
 						interval_price = msg.price_values[0]
-						price_set = True
 				else:
 					"Connection closed unexpectedly."
 					exit()
@@ -46,6 +42,7 @@ def main():
 			# Do something with the price signal
 			normal_energy_demand = 3000
 			curtailed_energy_demand = 1000
+			threshold_price = 300
 			if interval_price > threshold_price:
 				print("Price threshold exceeded. Shedding load.")
 				energy_demand = curtailed_energy_demand
@@ -55,13 +52,14 @@ def main():
 
 			# Report energy demand
 			session.report_power(energy_demand)
+			report_str = "S2Sim Price: {0}\nThreshold Price: {1}\nEnergy Demand: {2}\n"
+			print(report_str.format(interval_price, threshold_price, energy_demand)) 
 			sleep(1)
 
 	except KeyboardInterrupt:
 		print("\n\nExiting...")
 		session.s2sim_disconnect()
 		exit()
-
 
 if __name__=="__main__":
 	main()
